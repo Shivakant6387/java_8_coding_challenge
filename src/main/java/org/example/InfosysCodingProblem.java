383f9766fd855547ec15b13df3cfe95c6627853d
Shivakant6387 <shivakant5875@gmail.com>
2023-05-19 13:31:57 +0530

    infosys coding round

---
+++ src/main/java/org/example/InfosysCodingProblem.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.StringJoiner;
+
+/*A,B,C
+P:Q
+A,B,C,P:Q
+ */
+public class InfosysCodingProblem {
+    public static void main(String[] args) {
+        StringJoiner stringJoiner=new StringJoiner(",");
+        stringJoiner.add("A").add("B").add("C");
+        System.out.println(stringJoiner);
+        StringJoiner stringJoiner2=new StringJoiner(":");
+        stringJoiner2.add("P").add("Q");
+        System.out.println(stringJoiner2);
+        stringJoiner.merge(stringJoiner2);
+        System.out.println(stringJoiner);
+
+    }
+}
